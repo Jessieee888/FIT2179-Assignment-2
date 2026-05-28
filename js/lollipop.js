08e26d53392501b633bffffb635ad48b96410e69
Jesslyn Susanto <jesslynsusanto88@gmail.com>
2026-05-28 15:37:12 +1000

    try to add annotations to lollipop

--- js/lollipop.js
+++ js/lollipop.js
@@ -24,13 +24,21 @@ function renderLollipop() {
   fetch("vega/lollipop.json")
     .then(r => r.json())
     .then(spec => {
-      // Inject main data
       spec.data.values = values;
-
-      // Inject national average into the rule and label layers (layers 3 and 4)
       spec.layer[3].data.values = [{ avg: nationalAvg }];
       spec.layer[4].data.values = [{ avg: nationalAvg, label: "National avg. " + nationalAvg + "%" }];
 
       return vegaEmbed("#vis-lollipop", spec, { actions: false });
+    })
+    .then(() => {
+      const container = document.getElementById("vis-lollipop");
+      document.querySelectorAll("#vis-lollipop .map-annotation").forEach(el => el.remove());
+
+      const div = document.createElement("div");
+      div.className  = "map-annotation";
+      div.style.left = "72%";
+      div.style.top  = "7%";
+      div.innerHTML  = `<div class="map-annotation-bubble">ACT leads all states with the highest share<br><span style="font-size:0.62rem;opacity:0.8">More than 1 in 3 ACT schools are non-government</span></div>`;
+      container.appendChild(div);
     });
 }
